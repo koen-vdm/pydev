f32fc7f53e6f4688ea2a2933c1b55896af613b36
kvandenm <koen.van-den-maagdenberg.xn@renesas.com>
2023-06-12 12:19:54 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-docker build . -t koenvdm/pydev
+PROJNAME="pydev-lpcdocs"
+
+read -p "Enter the docker user to deploy to: " USER
+
+CURRENTUSER=$(docker info | sed '/Username:/!d;s/.* //')
+
+if [[ "$CURRENTUSER" != "$USER" ]]; then
+    docker login -u $USER
+fi
 
 if [[ "$?" -eq 0 ]]; then
-    docker push koenvdm/pydev
+    docker build . -t "$USER/$PROJNAME"
+fi
+
+if [[ "$?" -eq 0 ]]; then
+    docker push "$USER/$PROJNAME"
 fi
